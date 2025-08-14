05b79d64b68379cb04aa76bf974745c7a4b9fb3c
andrey.kononov <andrey.kononov@dsr-corporation.com>
2025-08-14 20:42:08 +0300

    Set test timeout for all tests over setup.ts

--- packages/hedera/tests/integration/HederaDidResolver.e2e.test.ts
+++ packages/hedera/tests/integration/HederaDidResolver.e2e.test.ts
@@ -3,8 +3,6 @@ import { HederaDidCreateOptions } from '../../src/ledger/HederaLedgerService'
 import { getHederaAgent } from './utils'
 
 describe('Hedera DID resolver', () => {
-  jest.setTimeout(100000)
-
   const logger = new ConsoleLogger(LogLevel.error)
 
   let agent: Agent
