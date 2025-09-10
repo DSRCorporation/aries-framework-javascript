74d91045ab4e516cfaf23f1f2827e0a6b9531b91
Alexander Shenshin <alexander.shenshin@dsr-corporation.com>
2025-09-11 02:50:20 +0300

    Clean up
    Signed-off-by: Alexander Shenshin <alexander.shenshin@dsr-corporation.com>

--- packages/hedera/tests/integration/utils/hederaModule.ts
+++ packages/hedera/tests/integration/utils/hederaModule.ts
@@ -9,23 +9,28 @@ import {
   HederaModuleConfigOptions,
 } from '@credo-ts/hedera'
 import { agentDependencies } from '@credo-ts/node'
+import { HederaNetwork } from '@hiero-did-sdk/client'
 import { anoncreds } from '@hyperledger/anoncreds-nodejs'
 import { askar } from '@openwallet-foundation/askar-nodejs'
 import { InMemoryTailsFileService } from '../../../../anoncreds/tests/InMemoryTailsFileService'
 
-export const getHederaModuleConfig = (props: { operatorId?: string; operatorKey?: string }) => {
-  const operatorId = props.operatorId ?? process.env.HEDERA_OPERATOR_ID ?? ''
-  const operatorKey = props.operatorKey ?? process.env.HEDERA_OPERATOR_KEY ?? ''
-  const config: HederaModuleConfigOptions = {
+export const getHederaModuleConfig = (props: {
+  network?: HederaNetwork
+  operatorId?: string
+  operatorKey?: string
+}): HederaModuleConfigOptions => {
+  return {
     networks: [
       {
-        network: 'testnet',
-        operatorId,
-        operatorKey,
+        network: props.network ?? (process.env.HEDERA_NETWORK as HederaNetwork) ?? 'testnet',
+        operatorId: props.operatorId ?? process.env.HEDERA_OPERATOR_ID ?? '0.0.5489553',
+        operatorKey:
+          props.operatorKey ??
+          process.env.HEDERA_OPERATOR_KEY ??
+          '302e020100300506032b6570042204209f54b75b6238ced43e41b1463999cb40bf2f7dd2c9fd4fd3ef780027c016a138',
       },
     ],
   }
-  return config
 }
 
 export const getHederaAgent = (props: {
